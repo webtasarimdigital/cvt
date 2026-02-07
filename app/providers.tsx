271e59f540900811372ef89e8f23f39f442b8ba0
webtasarimdigital <omerfarukamazon@gmail.com>
2026-02-07 12:26:53 +0300

    Refactor: Extract Providers component to fix build context issues

---
+++ app/providers.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { LanguageProvider } from "./context/LanguageContext";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <LanguageProvider>
+            {children}
+        </LanguageProvider>
+    );
+}
